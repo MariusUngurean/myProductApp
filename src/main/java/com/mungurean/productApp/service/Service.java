cab75bd7dbd605c544f1b9ee66cc5de91cc75f3a
MariusUngurean <Marius.Ungurean@endava.com>
2018-10-25 13:03:48 +0300

    serivce uses @transactional (try's removed)

--- src/main/java/com/mungurean/productApp/service/Service.java
+++ src/main/java/com/mungurean/productApp/service/Service.java
@@ -20,212 +20,88 @@ import java.util.Set;
 public class Service {
     private DAOImpl dao;
 
-    @Autowired
-    private EntityTransaction transaction;
-
     public Service() {
 
     }
 
     public Service(DAOImpl dao, EntityManager entityManager) {
         this.dao = dao;
-//        transaction = entityManager.getTransaction();
     }
 
     public void showAllProducts() {
-        try {
-            transaction.begin();
-            System.out.println("Here is a list of all the products");
-            List<Product> productList = dao.getAllProducts();
-            for (Product product : productList
-            ) {
-                System.out.println(product);
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
+        System.out.println("Here is a list of all the products");
+        List<Product> productList = dao.getAllProducts();
+        for (Product product : productList
+        ) {
+            System.out.println(product);
         }
     }
 
     public void showAllDescriptions() {
-        try {
-            transaction.begin();
-            System.out.println("Here is a list of all the descriptions");
-            List<Description> descriptionList = dao.getAllDescriptions();
-            for (Description description : descriptionList
-            ) {
-                System.out.println(description);
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
+        System.out.println("Here is a list of all the descriptions");
+        List<Description> descriptionList = dao.getAllDescriptions();
+        for (Description description : descriptionList
+        ) {
+            System.out.println(description);
         }
     }
 
     public void showAllCategories() {
-        try {
-            transaction.begin();
-            System.out.println("Here is a list of all the categories");
-            List<Category> categoryList = dao.getAllCategories();
-            for (Category category : categoryList
-            ) {
-                System.out.println(category);
-            }
-            transaction.commit();
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
+        System.out.println("Here is a list of all the categories");
+        List<Category> categoryList = dao.getAllCategories();
+        for (Category category : categoryList
+        ) {
+            System.out.println(category);
         }
     }
 
     public void showAllPrices() {
-        try {
-            transaction.begin();
-            System.out.println("Here is a list of all the products");
-            System.out.println(dao.getAllPrices());
-            transaction.commit();
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
+        System.out.println("Here is a list of all the products");
+        System.out.println(dao.getAllPrices());
     }
 
     public void addProduct(Product product) {
-        try {
-            transaction.begin();
-            dao.addProduct(product);
-            transaction.commit();
-            System.out.println("New product added");
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
-
+        dao.addProduct(product);
+        System.out.println("New product added");
     }
 
     public void addCategory(Category category) {
-//        try {
-//            transaction.begin();
         dao.addCategory(category);
-//        transaction.commit();
         System.out.println("New category added");
-//        } catch (Exception e) {
-//            e.getStackTrace();
-//            if (transaction != null && transaction.isActive()) {
-//                transaction.rollback();
-//            }
-//        }
-
     }
 
     public void updateProduct(long id, String name, String descriptionFlavorText, Category category, Set<Price> prices) {
-        try {
-            transaction.begin();
-            dao.updateProduct(id, name, descriptionFlavorText, category, prices);
-            transaction.commit();
-            System.out.println("Product has been updated");
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null && transaction.isActive())
-                transaction.rollback();
-        }
+        dao.updateProduct(id, name, descriptionFlavorText, category, prices);
+        System.out.println("Product has been updated");
     }
 
     public void updateCategory(long id, String name) {
-        try {
-            transaction.begin();
-            dao.updateCategory(id, name);
-            transaction.commit();
-            System.out.println("Category updated");
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
-
+        dao.updateCategory(id, name);
+        System.out.println("Category updated");
     }
 
     public void updateDescription(long id, String newFlavorText) {
-        try {
-            transaction.begin();
-            dao.updateDescription(id, newFlavorText);
-            transaction.commit();
-            System.out.println("Description updated");
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
-
+        dao.updateDescription(id, newFlavorText);
+        System.out.println("Description updated");
     }
 
     public void updatePrice(long id, double newValue, String newDate) {
-        try {
-            transaction.begin();
-            dao.updatePrice(id, newValue, newDate);
-            transaction.commit();
-            System.out.println("Price updated");
-        } catch (Exception e) {
-            e.getStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
-
+        dao.updatePrice(id, newValue, newDate);
+        System.out.println("Price updated");
     }
 
     public void deleteProduct(long id) {
-        try {
-            transaction.begin();
-            dao.deleteProduct(id);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
+        dao.deleteProduct(id);
     }
 
 
     public void deleteCategory(long id) {
-        try {
-            transaction.begin();
-            dao.deleteCategory(id);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
+        dao.deleteCategory(id);
+
     }
 
     public void deletePrice(long id) {
-        try {
-            transaction.begin();
-            dao.deletePrice(id);
-            transaction.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-        }
+        dao.deletePrice(id);
     }
 
     public Set<Price> pricesFromStringWithComaSeparator(String prices, Price firstPrice) {
